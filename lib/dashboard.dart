4ff01c56bb2e8872c3c7cf7e471b5a9a700bcc68
Saikat141 <72371927+Saikat141@users.noreply.github.com>
2025-07-25 18:01:54 +0600

    firebase connection created and files add

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class DashboardPage extends StatefulWidget {
   @override
   _DashboardPageState createState() => _DashboardPageState();
+
 }
 
 class _DashboardPageState extends State<DashboardPage> {
